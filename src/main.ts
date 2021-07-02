2028aea7b1e9b5d0d71074ce7a08a4d905bb30b6
Lan <15669147150@163.com>
2021-07-02 18:32:08 +0800

    新增错误捕获

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as express from 'express';
+import { logger } from './middleware/logger.middleware';
+import { TransformInterceptor } from './interceptor/transform.interceptor';
+import { HttpExceptionFilter } from './filter/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(express.json()); // For parsing application/json
+  app.use(express.urlencoded({ extended: true })); // For parsing
+  app.use(logger);
+  app.useGlobalInterceptors(new TransformInterceptor());
+  // 过滤处理 HTTP 异常
+  app.useGlobalFilters(new HttpExceptionFilter());
   // app.setGlobalPrefix('nest-zero-to-one');
   await app.listen(3000);
 }
