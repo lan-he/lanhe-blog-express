92e0906469c9994462cc76da398021eb31366737
Lan <15669147150g@gmail.com>
2024-09-13 16:57:23 +0800

    set

--- README.md
+++
@@ -1,9 +0,0 @@
-## express 项目
-
-### 启动
-
-`yarn dev` or `npm run dev`
-
-### 使用的库
-
-jsonwebtoken
--- app.js
+++ app.js
@@ -1,72 +1,41 @@
-var createError = require('http-errors')
-var express = require('express')
-var path = require('path')
-var cookieParser = require('cookie-parser')
-var logger = require('morgan')
-var indexRouter = require('./routes/index')
-var userRouter = require('./routes/user')
-var expandRouter = require('./routes/expand')
-const JWT = require('./util/jwt')
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-var app = express()
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
-app.use(logger('dev'))
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
-app.use(cookieParser())
-app.use(express.static(path.join(__dirname, 'public')))
-app.use((req, res, next) => {
-    // token校验中间件
-    if (req.url.includes('login') || req.url.includes('logon')) {
-        next()
-        return
-    }
-    const token = req.headers['authorization']
-    if (token && JWT.verify(token)) {
-        next()
-    } else {
-        res.send({
-            code: 401,
-        })
-    }
-})
-// 注册session中间件
-// app.use(
-//     session({
-//         name: 'express-test',
-//         secret: 'express-test-session', // 服务器生成 session 的签名
-//         resave: true, // true 为请求重制时间
-//         saveUninitialized: true, //强制将为初始化的 session 存储
-//         cookie: {
-//             maxAge: 1000 * 60 * 10, // 过期时间
-//             secure: false, // 为 true 时候表示只有 https 协议才能访问cookie
-//         },
-//     })
-// )
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter)
-app.use('/api/user', userRouter)
-app.use('/api/expand', expandRouter)
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
-    next(createError(404))
-})
+app.use(function(req, res, next) {
+  next(createError(404));
+});
 
 // error handler
-app.use(function (err, req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*')
-    // set locals, only providing error in development
-    res.locals.message = err.message
-    res.locals.error = req.app.get('env') === 'development' ? err : {}
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-    // render the error page
-    res.status(err.status || 500)
-    res.render('error')
-})
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
-module.exports = app
+module.exports = app;
