4bc158952cbb6d2f17771f6d8fc60bea88b35fbf
Lan <15669147150g@gmail.com>
2024-09-13 17:04:12 +0800

    新增接口

--- app.js
+++ app.js
@@ -1,41 +1,70 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require('http-errors')
+var express = require('express')
+var path = require('path')
+var cookieParser = require('cookie-parser')
+var logger = require('morgan')
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const { MongoClient, ServerApiVersion } = require('mongodb')
+const uri =
+    'mongodb+srv://15669147150g:89ta6e3b6ojHL02D@cluster0.2irj1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    },
+})
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect()
+        // Send a ping to confirm a successful connection
+        await client.db('admin').command({ ping: 1 })
+        console.log(
+            'Pinged your deployment. You successfully connected to MongoDB!'
+        )
+    } finally {
+        // Ensures that the client will close when you finish/error
+        await client.close()
+    }
+}
+run().catch(console.dir)
 
-var app = express();
+var indexRouter = require('./routes/index')
+var usersRouter = require('./routes/users')
+var blogRouter = require('./routes/blog')
+
+var app = express()
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'jade')
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', indexRouter)
+app.use('/users', usersRouter)
+app.use('/blog', blogRouter)
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+app.use(function (req, res, next) {
+    next(createError(404))
+})
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message
+    res.locals.error = req.app.get('env') === 'development' ? err : {}
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+    // render the error page
+    res.status(err.status || 500)
+    res.render('error')
+})
 
-module.exports = app;
+module.exports = app
---
+++ routes/blog.js
@@ -0,0 +1,57 @@
+var express = require('express')
+var router = express.Router()
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb')
+
+// MongoDB 连接 URI
+const uri =
+    'mongodb+srv://15669147150g:89ta6e3b6ojHL02D@cluster0.2irj1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+
+// 创建 MongoClient 实例并配置 Server API 版本
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    },
+})
+// 连接数据库的函数
+async function connectDB() {
+    try {
+        await client.connect()
+        const db = client.db('blog') // 连接到名为 "blog" 的数据库
+        return db
+    } catch (error) {
+        console.error('Error connecting to MongoDB:', error)
+    }
+}
+router.post('/articles/add', async (req, res) => {
+    const { title, content, describe } = req.body
+
+    if (!title || !content) {
+        return res
+            .status(400)
+            .json({ message: 'Title and content are required' })
+    }
+
+    try {
+        const db = await connectDB()
+        const articlesCollection = db.collection('articles')
+
+        const newArticle = {
+            title,
+            content,
+            describe: describe || '',
+            likes: 0,
+            views: 0,
+            createdAt: new Date(),
+            comments: [],
+        }
+
+        const result = await articlesCollection.insertOne(newArticle)
+        res.status(201).json({ articleId: result.insertedId })
+    } catch (error) {
+        res.status(500).json({ message: 'Error creating article', error })
+    }
+})
+
+module.exports = router
